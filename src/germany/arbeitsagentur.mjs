60c5eab578b367b2f85ebd484e67a6292c5ade19
ThanonC <126615459+ThanonC@users.noreply.github.com>
2025-06-05 06:55:00 +0000

    c

---
+++ src/germany/arbeitsagentur.mjs
@@ -0,0 +1,8 @@
+//https://github.com/bundesAPI/ausbildungssuche-api
+
+export default async function find(sty, loc, region, range) {
+    const url = `https://rest.arbeitsagentur.de/infosysbub/absuche/pc/v1/ausbildungsangebot?sty=${sty}&orte=${loc}&re=${region}&uk=${range}`;
+    try {
+        
+    }
+}
